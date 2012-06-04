72acf453e89ea49cb1f2bb7fd30a68dd71fd7b04
James Blanding <ilikepi+github@gmail.com>
2012-06-04 18:16:02 -0400

    postgis14: formatting tweaks                     *
    source[0]: mxcl/homebrew@120030b

--- postgis14.rb
+++ postgis14.rb
@@ -11,18 +11,29 @@ class Postgis <Formula
 
   def install
     ENV.deparallelize
-    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking", "--with-projdir=#{HOMEBREW_PREFIX}"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-projdir=#{HOMEBREW_PREFIX}"
     system "make install"
 
     # Copy some of the generated files to the share folder
-    (share+'postgis').install %w( spatial_ref_sys.sql postgis/postgis.sql postgis/postgis_upgrade.sql postgis/uninstall_postgis.sql )
+    (share+'postgis').install %w(
+      spatial_ref_sys.sql postgis/postgis.sql
+      postgis/postgis_upgrade.sql
+      postgis/uninstall_postgis.sql
+    )
     # Copy loader and utils binaries to bin folder
-    bin.install %w( loader/pgsql2shp loader/shp2pgsql utils/new_postgis_restore.pl utils/postgis_proc_upgrade.pl utils/postgis_restore.pl utils/profile_intersects.pl )
+    bin.install %w(
+      loader/pgsql2shp loader/shp2pgsql
+      utils/new_postgis_restore.pl utils/postgis_proc_upgrade.pl
+      utils/postgis_restore.pl utils/profile_intersects.pl
+    )
 
   end
 
-  def caveats
-    <<-EOS.undent
+  def caveats; <<-EOS.undent
+    Postgresql 9.0 is not supported by PostGis 1.4.
+
       To create a spatially-enabled database, see the documentation:
         http://postgis.refractions.net/documentation/manual-1.4/ch02.html#id2754935
     EOS
