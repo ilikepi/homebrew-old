62185bb55715d528b7f61d291efe4a35a55b07fc
James Blanding <ilikepi+github@gmail.com>
2014-02-14 16:36:20 -0500

    ruby-install034: fix module name

--- ruby-install034.rb
+++ ruby-install034.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class RubyInstall < Formula
+class RubyInstall034 < Formula
   homepage 'https://github.com/postmodern/ruby-install#readme'
   url 'https://github.com/postmodern/ruby-install/archive/v0.3.4.tar.gz'
   sha1 '76b1df95cb056cfe432499a780f6a12ce908b159'
