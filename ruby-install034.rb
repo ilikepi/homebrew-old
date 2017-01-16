669402ed56fbc09e06867ef1e949cccf922d8d1d
James Blanding <james@basicsgroup.com>
2017-01-16 12:51:58 -0500

    ruby-install034: update for Homebrew 1.0.0

--- ruby-install034.rb
+++ ruby-install034.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class RubyInstall034 < Formula
   homepage 'https://github.com/postmodern/ruby-install#readme'
   url 'https://github.com/postmodern/ruby-install/archive/v0.3.4.tar.gz'
-  sha1 '76b1df95cb056cfe432499a780f6a12ce908b159'
+  sha256 'ccb1fe7c598e6c309c13f2b5892659247c5d21f3b0fff516a68f923048ea9f89'
 
   head 'https://github.com/postmodern/ruby-install.git'
 
