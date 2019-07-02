fa144832a9b166cb2b013781d5850d0bd576da76
James Blanding <james@basicsgroup.com>
2019-07-02 15:12:48 -0400

    postgresql8.rb: rename formula

--- postgresql@8.4.rb
+++ postgresql@8.4.rb
@@ -1,4 +1,4 @@
-class Postgresql8 < Formula
+class PostgresqlAT84 < Formula
   desc "Object-relational database system"
   homepage "http://www.postgresql.org/"
   url "http://ftp.postgresql.org/pub/source/v8.4.22/postgresql-8.4.22.tar.bz2"
